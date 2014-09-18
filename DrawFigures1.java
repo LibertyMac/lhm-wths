62b918dcc362e1326146935bf46783494b701c22
LibertyMac <liberty.macmillan@techhigh.us>
2014-09-18 09:32:11 -0400

    I added a comment to it, thats all

--- DrawFigures1.java
+++ DrawFigures1.java
@@ -1,6 +1,6 @@
 public class DrawFigures1 {
     public static void main(String[] args) {
-        System.out.println("   /\\");
+        System.out.println("   /\\"); // this is a comment though I think you can tell already //
         System.out.println("  /  \\");
         System.out.println(" /    \\");
         System.out.println(" \\    /");
