82ab02c5ac3bc3b36cd2cdc1d1fb5baade4164e8
unknown <macmillanl@B219-013.techhigh.local>
2014-09-19 07:55:20 -0400

    i added comments to the code

--- DrawFigures1.java
+++ DrawFigures1.java
@@ -1,6 +1,7 @@
 public class DrawFigures1 {
     public static void main(String[] args) {
-        System.out.println("   /\\");
+    
+		System.out.println("   /\\");		//wooo comments//
         System.out.println("  /  \\");
         System.out.println(" /    \\");
         System.out.println(" \\    /");
@@ -14,7 +15,7 @@ public class DrawFigures1 {
         System.out.println("  /  \\");
         System.out.println(" /    \\");
         System.out.println();
-        System.out.println("   /\\");
+        System.out.println("   /\\");			//look ma no hands!//
         System.out.println("  /  \\");
         System.out.println(" /    \\");
         System.out.println("+------+");
@@ -22,7 +23,7 @@ public class DrawFigures1 {
         System.out.println("|      |");
         System.out.println("+------+");
         System.out.println("|United|");
-        System.out.println("|States|");
+        System.out.println("|States|");			// <- not my code //
         System.out.println("+------+");
         System.out.println("|      |");
         System.out.println("|      |");
