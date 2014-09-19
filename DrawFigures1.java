0d573321e4eaecaa9d004f849e63d49995eb9112
unknown <macmillanl@B219-013.techhigh.local>
2014-09-19 08:06:16 -0400

    I applied procedural decomposition to the project

--- DrawFigures1.java
+++ DrawFigures1.java
@@ -1,35 +1,34 @@
 public class DrawFigures1 {
     public static void main(String[] args) {
-    
+    ditop()
 		System.out.println("   /\\");		//wooo comments//
         System.out.println("  /  \\");
         System.out.println(" /    \\");
-        System.out.println(" \\    /");
-        System.out.println("  \\  /");
-        System.out.println("   \\/");
-        System.out.println();
-        System.out.println(" \\    /");
-        System.out.println("  \\  /");
-        System.out.println("   \\/");
-        System.out.println("   /\\");
-        System.out.println("  /  \\");
-        System.out.println(" /    \\");
-        System.out.println();
-        System.out.println("   /\\");			//look ma no hands!//
-        System.out.println("  /  \\");
-        System.out.println(" /    \\");
-        System.out.println("+------+");
+	box()
+		System.out.println("+------+");
         System.out.println("|      |");
         System.out.println("|      |");
         System.out.println("+------+");
+	dibot()
+		System.out.println(" \\    /");
+        System.out.println("  \\  /");
+        System.out.println("   \\/");
+	
+		
+				
+        System.out.println(ditop);
+        System.out.println(dibot);
+
+        System.out.println();
+		System.out.println(dibot);
+        System.out.println(ditop);
+        
+        System.out.println();
+        System.out.println(ditop);			//look ma no hands!//
+        System.out.println(box);
         System.out.println("|United|");
         System.out.println("|States|");			// <- not my code //
-        System.out.println("+------+");
-        System.out.println("|      |");
-        System.out.println("|      |");
-        System.out.println("+------+");
-        System.out.println("   /\\");
-        System.out.println("  /  \\");
-        System.out.println(" /    \\");
+		System.out.println(box);
+        System.out.println(ditop);
     }
 }
